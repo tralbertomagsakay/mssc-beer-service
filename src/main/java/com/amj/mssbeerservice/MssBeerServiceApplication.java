3e5a2332c6ed559c97a13a412f76fc9471794391
Alberto Magsakay <alberto.magsakay@tooltwist.com>
2020-02-13 13:40:30 +0800

    Initial

---
+++ src/main/java/com/amj/mssbeerservice/MssBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amj.mssbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MssBeerServiceApplication.class, args);
+    }
+
+}
